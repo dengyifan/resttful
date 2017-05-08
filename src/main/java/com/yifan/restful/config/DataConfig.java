22f614bb9934d44d8bbd6739889ae7ade0f44896
yifan <waynesilver@163.com>
2017-05-08 16:45:13 +0800

    添加数据库依赖 添加 DataConfig

---
+++ src/main/java/com/yifan/restful/config/DataConfig.java
@@ -0,0 +1,10 @@
+package com.yifan.restful.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by yifan on 2017/5/8.
+ */
+@Configuration
+public class DataConfig {
+}
--- src/main/java/com/yifan/restful/config/RootConfig.java
+++ src/main/java/com/yifan/restful/config/RootConfig.java
@@ -7,6 +7,7 @@ package com.yifan.restful.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
@@ -26,5 +27,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @ComponentScan(basePackages = {"com.yifan.**.dao"},
         excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)})
+@Import(DataConfig.class)
 public class RootConfig {
 }
