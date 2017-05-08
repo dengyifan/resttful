c5d758b197671dd251362e12af48059313e5e574
yifan <waynesilver@163.com>
2017-05-08 16:50:07 +0800

    配置 sqlSessionFactory

--- src/main/java/com/yifan/restful/config/DataConfig.java
+++ src/main/java/com/yifan/restful/config/DataConfig.java
@@ -1,10 +1,41 @@
 package com.yifan.restful.config;
 
+import org.apache.commons.dbcp.BasicDataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 /**
  * Created by yifan on 2017/5/8.
  */
 @Configuration
 public class DataConfig {
+
+    @Bean
+    public BasicDataSource dataSource(){
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/dbmvc?useUnicode=true&characterEncoding=utf-8");//防止乱码
+        dataSource.setUsername("root");
+        dataSource.setPassword("1234");
+        dataSource.setInitialSize(5);
+        dataSource.setMaxActive(10);
+        return dataSource;
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setTypeAliasesPackage("com.yifan.**.domain");
+        sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:com/yifan/**/dao/*.xml"));
+        return sessionFactory;
+    }
 }
